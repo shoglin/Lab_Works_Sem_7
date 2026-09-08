b5eb735c9dd8b561e23c24e5272a0a9cc8a40c86
shoglin <shoglin@list.ru>
2026-09-08 12:49:16 +0300

    Initial punch in head

---
+++ LW1/untitled3.m
@@ -0,0 +1,18 @@
+W1 = tf(0.0067, [1 0])
+W2 = 21;
+W3 = tf(14, [0.15 1])
+W4 = 36;
+W5 = 59;
+W6 = tf(1, [1 0]);
+
+W12 = feedback(W1, W2, -1);
+W123 = series(W3, W12);
+W1234 = feedback(W123, W4, -1);
+W123456 = series(W1234, W6)*W5;
+W7 = feedback(W123456, 1, -1);
+
+figure; 
+step(W7);
+figure; 
+margin(W123456);
+
